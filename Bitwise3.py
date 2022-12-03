bc92526b52bf7929a8203e1c7b46ac461431d2c7
Altaf Najillah <muhaltafnajillah@gmail.com>
2022-12-03 20:05:52 +0800

    Hari Ke-48

--- Bitwise3.py
+++ Bitwise3.py
@@ -1,14 +1,15 @@
-'''Program Menjalankan Operasi AND pada Operator Bitwise'''
+'''Program Menjalankan Operasi XOR pada Operator Bitwise'''
 
-print('Operator Bitwise AND')
+
+print('Operator Bitwise XOR')
 
 print('Masukkan dua angka yang akan dioperasikan')
 input1 = int(input('Angka Pertama\t: '))
 input2 = int(input('Angka Kedua\t: '))
-bitwise = input1 & input2
+bitwise = input1 ^ input2
 
 print('Angka', input1, 'memiliki binary : ', format(input1, '08b'))
 print('Angka', input2, 'memiliki binary : ', format(input2, '08b'))
 
-print('Hasil Bitwise \"OR\"')
+print('Hasil Bitwise \"XOR\"')
 print('Binary :', format(bitwise, '08b'), ", Angka : ", bitwise)
