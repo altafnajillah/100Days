5870ca2a5b3c6439709c23ce2d4af3c0c5cfbdad
Altaf Najillah <muhaltafnajillah@gmail.com>
2022-12-25 22:37:16 +0800

    Hari ke-69

--- Bil.Prima.py
+++ Bil.Prima.py
@@ -5,7 +5,7 @@ print("Program mengecek bilangan prima")
 
 angka = int(input("Masukkan sebuah bilangan bulat : "))
 
-if (angka == 1 or angka == 2 or angka == 3 or angka == 5 or angka % 2 != 0 and angka % 3 != 0 and angka % 5 != 0 and angka % 7 != 0):
+if (angka == 1 or angka == 2 or angka == 3 or angka == 5 or angka == 7 or angka % 2 != 0 and angka % 3 != 0 and angka % 5 != 0 and angka % 7 != 0):
     print("Bilangan {} merupakan bilangan prima".format(angka))
 else :
     print("Bilangan {} BUKAN merupakan bilangan prima".format(angka))
